a535d6cee0e023f167cac7fa1ca8c6b0c755beb6
hp <hp@DESKTOP-CC4ASUH>
2024-10-09 00:01:39 -0700

    My first commit

---
+++ src/main/java/com/test/p1/testProgram.java
@@ -0,0 +1,11 @@
+package com.test.p1;
+
+public class testProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hy from eclipse....");
+
+	}
+
+}
